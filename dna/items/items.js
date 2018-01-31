0d1fffda42dff246eb0e97c2185131ec727ce25b
dholmes <silversnayke@gmail.com>
2018-01-31 15:54:39 +0000

    added items folder, added update item function, renamed testfile

--- dna/items/items.js
+++ dna/items/items.js
@@ -6,69 +6,10 @@ var wikiName = "TheFederation"
 // https://developer.holochain.org/API_reference
 // https://developer.holochain.org/Test_driven_development_features
 
-function createPage (arg) {
-  var pageObject = arg;
-  pageObject["wikiName"] = wikiName;
-  var hash = commit("page", pageObject);
-  return hash;
-}
-
-function renamePage (arg) {
-  var newPage = {title: arg.newEntry.title, wikiName: wikiName};
-  var hash = update("page", newPage, arg.hashKey);
-  return hash;
-}
-
-function getPage (hash) {
-  var page = get(hash);
-  return JSON.parse(page);
-}
-
-function addItem (arg) {
-  var pageHash = arg.pageHash;
+function updateItem (arg) {
   var newItem = arg.newItem;
-  var itemHash = commit("item", newItem);
-  // create link between pageHash and itemHash
-  var pageLinkHash = commit("pageLinks", {
-    Links: [
-      {Base:pageHash, Link:itemHash, Tag:"page item"}
-    ]
-  });
-  return itemHash;
-}
-
-function getFedWikiJSON (pageHash) {
-  // should look like: http://connor.outlandish.academy/start-here.json
-  var page = JSON.parse(get(pageHash));
-  var pageLinks = getLinks(pageHash, "page item", { Load: true });
-
-  // define a top level object for our response
-  var response = {
-    title: page.title
-  };
-  // we will populate this story array with items
-  var story = [];
-
-  for (var i = 0; i < pageLinks.length; i++) {
-    // get the basic entry object
-    var item = pageLinks[i].Entry;
-    // create a new item which will be put into the story
-    var newItem = {
-      type: item.type,
-      id: item.id
-    };
-    // pull extra, type-specific fields up
-    for (var field in item.fields) {
-      if (item.fields.hasOwnProperty(field)) {
-        newItem[field] = item.fields[field]
-      }
-    }
-    // add the properly item structured into the story array
-    story.push(newItem);
-  }
-  // set the story array as a property on the response
-  response.story = story;
-  return response;
+  var hash = update("item", newItem, arg.hashKey);
+  return hash;
 }
 
 // VALIDATION FUNCTIONS
